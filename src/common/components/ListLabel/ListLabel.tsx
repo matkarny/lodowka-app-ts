13be007aa00675a1606aff178470a47073b321db
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-09 14:17:31 +0200

    Add ListLabel component

---
+++ src/common/components/ListLabel/ListLabel.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import './ListLabel.scss';
+
+export interface ListLabelProps {
+    labelTxt: string
+}
+
+export interface ListLabelState {
+    itemsCount: number
+}
+
+class ListLabel extends React.Component<ListLabelProps, ListLabelState> {
+    state = { itemsCount: 9 }
+    render() {
+        return (<div className="list-label">
+            <span className='list-label__title'>
+                {this.state.itemsCount} {this.props.labelTxt}
+            </span>
+            <div className="list-label__btn-container">
+                {this.props.children}
+            </div>
+        </div>);
+    }
+}
+
+export default ListLabel;
