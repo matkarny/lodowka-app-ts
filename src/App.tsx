33e8fae0c842389458914926c4312abe4f23881a
Mateusz Karny <mattmaister@gmail.com>
2019-07-11 13:42:39 +0200

    Dashboard template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DashboardModule from './modules/DashboardModule/DashboardModule'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
+    <div>
+<DashboardModule />
     </div>
   );
 }
