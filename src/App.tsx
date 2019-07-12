cc0f10afa3a1179a722fb0177ae5942bfb02556e
Mateusz Karny <mattmaister@gmail.com>
2019-07-12 13:04:16 +0200

    Initial product list

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const App: React.FC = () => {
         <DrawingComponent />
         <YoutubeWidget />
       </DashboardModule>
+
     </div>
   );
 }
---
+++ src/actions/ProductExpireChecker/ProductExpireChecker.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class ProductExpireChecker extends Component {
+
+    currentDate = 
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
