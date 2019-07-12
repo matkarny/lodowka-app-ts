73aca1c9a3a36701010b29a3426f9287ba57259c
Mateusz Karny <mattmaister@gmail.com>
2019-07-12 10:50:18 +0200

    Ready clock widget

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import './App.css';
 import DashboardModule from './modules/DashboardModule/DashboardModule';
 import YoutubeWidget from './modules/YoutubeWidget/YoutubeWidget';
 import DrawingComponent from './common/components/DrawingComponent/DrawingComponent';
+import TimeWidget from './modules/TimeWidget/TimeWidget';
 
 const App: React.FC = () => {
   return (
-    <div>    
-      <DashboardModule> 
-        <DrawingComponent />
-        <YoutubeWidget />
-      </DashboardModule>
+    <div>   
+      <DashboardModule ><TimeWidget /></DashboardModule> 
 
     </div>
   );
