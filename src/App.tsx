77115fcb61e5ab067293a1f9df56792dbacc8171
Mateusz Karny <mattmaister@gmail.com>
2019-07-12 11:05:40 +0200

    New master

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,16 @@ import DashboardModule from './modules/DashboardModule/DashboardModule';
 import YoutubeWidget from './modules/YoutubeWidget/YoutubeWidget';
 import DrawingComponent from './common/components/DrawingComponent/DrawingComponent';
 import TimeWidget from './modules/TimeWidget/TimeWidget';
+import WeatherWidgetView from './modules/WeatherWidget/WeatherWidgetView';
 
 const App: React.FC = () => {
   return (
     <div>   
-      <DashboardModule ><TimeWidget /></DashboardModule> 
+      <DashboardModule ><TimeWidget />
+      <WeatherWidgetView />
+      <DrawingComponent />
+      <YoutubeWidget />
+      </DashboardModule> 
 
     </div>
   );
