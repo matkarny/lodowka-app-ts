60e4ba20ec4b323a1f89306b84c7a2369881b671
Jan Bednarek <jabedek@gmail.com>
2019-07-12 10:39:10 +0200

    GOT FRIDGE IMAGE!

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import DashboardModule from './modules/DashboardModule/DashboardModule';
 import WeatherWidgetView from './modules/WeatherWidget/WeatherWidgetView';
 import YoutubeWidget from './modules/YoutubeWidget/YoutubeWidget';
 import DrawingComponent from './common/components/DrawingComponent/DrawingComponent';
+import FridgeView from './modules/Fridge/FridgeView';
 
 const App: React.FC = () => {
   return (
     <div>
       <DashboardModule>
-        <DrawingComponent />
-        <WeatherWidgetView />
-        <YoutubeWidget />
+        <FridgeView />
       </DashboardModule>
     </div>
   );
--- src/modules/Fridge/FridgeView.tsx
+++ src/modules/Fridge/FridgeView.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
 import io from 'socket.io-client';
+import './Fridge.scss';
 
 export interface FridgeViewProps {}
 
 export interface FridgeViewState {
-  stuff: string;
+  src: string;
 }
 
 class FridgeView extends React.Component<FridgeViewProps, FridgeViewState> {
-  state = { stuff: '' };
+  state = { src: '' };
   test = () => {
     // let socket = io();
   };
+
+  getImageBase64() {
+    console.log('B64');
+    const script = document.createElement('script');
+    script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js';
+    script.async = true;
+    document.body.appendChild(script);
+    var socket = io('http://10.254.0.40:3000');
+    socket.on('image', image => {
+      const imageElm = document.querySelector('#image');
+      // console.log(imageElm);
+      this.setState({ src: `data:image/jpeg;base64,${image}` });
+    });
+  }
+
+  componentDidMount() {
+    this.getImageBase64();
+  }
+
   render() {
     return (
       <div>
-        <img src="http://10.254.0.40:3000/" alt="fridge" />
+        <img src={this.state.src} id="image" className="fridge__image" />
       </div>
     );
   }
