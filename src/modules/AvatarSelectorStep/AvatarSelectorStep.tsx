0b8bd66d0a17631fc594645a1437f8de37303289
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-15 19:52:00 +0200

    Rename component

--- src/modules/AvatarSelectorStep/AvatarSelectorStep.tsx
+++ src/modules/AvatarSelectorStep/AvatarSelectorStep.tsx
@@ -1,27 +1,16 @@
 import * as React from 'react';
-import './AvatarSelector.scss';
-import owl from './img/eagle-owl-raptor-falconry-owl-53977.png'
-import eagle from './img/pexels-photo-145962.png'
-import raccoon from './img/pexels-photo-148359.png'
-import parrot from './img/pexels-photo-1661179.png'
-import cat from './img/pexels-photo-2355519.png'
+import './AvatarSelectorStep.scss';
 export interface AvatarSelectorProps {
 
 }
 
 export interface AvatarSelectorState {
-    users: Array<object>,
+    selectedAvatar: number
 }
 
 class AvatarSelector extends React.Component<AvatarSelectorProps, AvatarSelectorState> {
     state = {
-        users: [],
-        newUser: {
-            isParent: true,
-            name: 'Janusz',
-            avatar: 0,
-            colorIndex: 0
-        }
+        selectedAvatar: null
     }
     render() {
         return (
