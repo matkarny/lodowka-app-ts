953908b1c847a1ca12bbc74b07a3c97fafe07b38
Mateusz Karny <mattmaister@gmail.com>
2019-07-16 12:15:19 +0200

    ListLabel Product List widget ready

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,12 @@ import YoutubeWidget from './modules/YoutubeWidget/YoutubeWidget';
 import DrawingComponent from './common/components/DrawingComponent/DrawingComponent';
 import TimeWidget from './modules/TimeWidget/TimeWidget';
 import WeatherWidgetView from './modules/WeatherWidget/WeatherWidgetView';
-import ProductExpireChecker from './actions/ProductExpireChecker/ProductExpireChecker';
-import ProductLabel from "./common/components/ProductLabel/ProductLabel"
+import ProductListWidget from './modules/ProductListWidget/ProductListWidget';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-<ProductLabel productName={"Makaron"}> <ProductExpireChecker productDay={1} productMounth={6} productYear={2019} /> </ProductLabel>
-<ProductLabel productName={"Ryż"}> <ProductExpireChecker productDay={1} productMounth={7} productYear={2019} /> </ProductLabel>
+<ProductListWidget />
     
 
     </div>
