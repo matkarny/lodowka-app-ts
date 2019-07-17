c3638f9f93fd1bfe18922dbe01accfd0768a3db2
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-17 14:18:19 +0200

    Add ConfirmationStep

---
+++ src/modules/ConfirmationStep/ConfirmationStep.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import './ConfirmationStep.scss'
+import avatar from '../../common/images/pexels-photo-148359@2x.png'
+export interface ConfirmationStepProps {
+    // color: string,
+    // avatarSRCHigh: string,
+    // username: string,
+}
+
+const ConfirmationStep: React.SFC<ConfirmationStepProps> = props => {
+    return (
+        <div className="login-confirmation__container">
+
+            <p className='login-confirmation__title'>Does that sound alright?</p>
+            <p className='login-confirmation__subtitle'>Check if everything is in order and confirm to create an account or go back to make changes.</p>
+
+            <div className="login-confirmation__items-wrapper">
+                <div className={`login-confirmation__color login-confirmation__color--`}>
+                    <img src={avatar} alt="" className="login-confirmation__avatar" />
+                    <p className="login-confirmation__username">Janusz</p>
+                </div>
+
+            </div>
+            {/* <div className="login-confirmation__btn-container">
+                <button
+                    className="login-confirmation__button login-confirmation__button--cancel"
+                    onClick={this.props.onBack}>Back</button>
+                <button
+                    className={`login-confirmation__button ${this.state.selectedconfirmation ? 'login-confirmation__button--select' : 'login-confirmation__button--disabled'}`}
+                    disabled={this.state.selectedconfirmation ? false : true}
+                    onClick={() => this.props.onSelect(this.state.selectedconfirmation)}>
+                    Select
+            </button>
+
+            </div> */}
+        </div>
+    );
+}
+
+export default ConfirmationStep;
