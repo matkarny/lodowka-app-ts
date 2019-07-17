1071273e3024872d6f1b0ecb11c4eaa52b783640
Mateusz Karny <mattmaister@gmail.com>
2019-07-17 12:04:12 +0200

    Initial NoteWidget

---
+++ src/common/components/FullView/FullView.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import "./FullView.scss"
+import * as Routes from "../../constants/Routes"
+
+export interface FullViewProps {
+    labelName: string,
+    firstButtonName: string,
+    firstButtonName: string,
+
+}
+
+export default class FullView extends React.Component<FullViewProps> {
+
+
+
+    public render() {
+        return (
+            <div className="full-list">
+                <header className="full-list__header">
+                    <a href={Routes.DASHBOARD} className="full-list__link"><div className="full-list__header full-list__arrow">🡠</div> </a>
+                    <div className="full-list__middle-container">
+                        <div className="full-list__middle-container-text">Your Products </div>
+                        <div className="full-list__buttons-list-container">
+                            <button className="full-list__buttons-list-container full-list__buttons-list-container-nonactive">buttons</button>
+                            <button className="full-list__buttons-list-container full-list__buttons-list-container-active">List</button>
+                        </div>
+                    </div>
+                    <div></div>
+                </header>
+                <div className="full-list__input">
+                </div>
+            </div>
+        );
+    }
+}
