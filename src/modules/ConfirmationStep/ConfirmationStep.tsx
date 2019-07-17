41642bb1885bf3ae65eb38a6150d99d03a793e9e
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-17 15:24:51 +0200

    Handle props

--- src/modules/ConfirmationStep/ConfirmationStep.tsx
+++ src/modules/ConfirmationStep/ConfirmationStep.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react';
 import './ConfirmationStep.scss'
 import avatarList from '../../common/constants/AnimalConstants';
-import avatar from '../../common/images/pexels-photo-148359@2x.png'
+import { COLORS } from '../../common/constants/ColorConstants';
 export interface ConfirmationStepProps {
-    // color: number,
+    colorId: number,
     avatarId: number,
-    // username: string,
+    username: string,
 }
 
 const ConfirmationStep: React.SFC<ConfirmationStepProps> = props => {
 
     // const chosenAvatar = avatarList.find(avatar => avatar.id === parseInt(props.avatarId))
     const chosenAvatar = avatarList.find(avatar => avatar.id === props.avatarId)
+    const chosenColor = COLORS.find(color => color.id === props.colorId)
 
     return (
         <div className="login-confirmation__container">
@@ -20,11 +21,11 @@ const ConfirmationStep: React.SFC<ConfirmationStepProps> = props => {
             <p className='login-confirmation__subtitle'>Check if everything is in order and confirm to create an account or go back to make changes.</p>
 
             <div className="login-confirmation__items-wrapper">
-                <div className={`login-confirmation__color login-confirmation__color--`}>
+                <div className={`login-confirmation__color login-confirmation__color--${chosenColor.name}`}>
                     <div className="login-confirmation__avatar-container">
                         <img src={chosenAvatar.srcHigh} className="login-confirmation__avatar" />
                     </div>
-                    <p className="login-confirmation__username">Janusz</p>
+                    <p className="login-confirmation__username">{props.username}</p>
                 </div>
 
             </div>
