f310f9fe56254ed9564a6546aa15a7a2f24fbf17
Mateusz Karny <mattmaister@gmail.com>
2019-07-18 15:58:39 +0200

    NoteLabel ready

--- src/modules/NotesFullView/NotesFullView.tsx
+++ src/modules/NotesFullView/NotesFullView.tsx
@@ -2,17 +2,13 @@ import * as React from 'react';
 import FullView from '../../common/components/FullView/FullView';
 import AddNoteComponent from "../../common/components/AddNoteComponent/AddNoteComponent"
 import { store } from '../../store/storeConfigure'
-import NoteLabel from '../../common/components/NoteLabel/NoteLabel'
-
 export interface INotesFullViewProps {
 }
 
 export default class NotesFullView extends React.Component<INotesFullViewProps> {
   public render() {
     return (
-      <div>
-      <NoteLabel  date={"18/07/2019"} message={"aSaxqwXASxwqsadasdsadasdasdasd"} />
-      </div>
+        <FullView startAtFirst={true} labelName={"Notes"} firstButtonName={"Show all notes"} secondButtonName={"Add new note"} firstComponent={<button > Store </button>} secondComponent={<AddNoteComponent />} />
     );
   }
 }
