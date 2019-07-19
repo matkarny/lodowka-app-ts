9c9b2e7f3276bf9f80edea4269d7f8fdad736765
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-19 09:59:48 +0200

    Add fetching data method

--- src/common/components/UserComponent/UserComponent.tsx
+++ src/common/components/UserComponent/UserComponent.tsx
@@ -1,29 +1,31 @@
 import * as React from 'react';
 import './UserComponent.scss';
+import avatarList from '../../../common/constants/AnimalConstants';
+import { COLORS } from '../../../common/constants/ColorConstants';
 
 export interface UserComponentProps {
-    chosenAvatar: object,
-    chosenColor: object,
+    avatarId: number,
+    colorId: number,
     username: string,
     bigger: boolean
 }
 
 const UserComponent: React.SFC<UserComponentProps> = props => {
-    const { chosenColor, chosenAvatar, bigger, username } = props;
+    const { colorId, avatarId, bigger, username } = props;
+
+    const chosenAvatar = avatarList.find(avatar => avatar.id == avatarId)
+    const chosenColor = COLORS.find(color => color.id == colorId)
     return (
         <div className="user-component__items-wrapper">
-            <div className={`user-component__color user-component__color--${chosenColor.name} ${bigger ? 'user-component__color' : ''}`}>
+            <div className={`user-component__color user-component__color--${chosenColor.name} ${bigger ? 'user-component__color--bigger' : ''}`}>
                 <div className={`user-component__avatar-container ${bigger ? 'user-component__avatar-container--bigger' : ''}`}>
                     <img src={`${bigger ? chosenAvatar.srcHigh : chosenAvatar.src}`} className="user-component__avatar" />
                 </div>
-                <p className={`user-component__username ${bigger ? 'user-component__username' : ''}`}>{username}</p>
+                <p className={`user-component__username ${bigger ? 'user-component__username--bigger' : ''}`}>{username}</p>
             </div>
         </div >
 
     );
-}
-UserComponent.defaultProps = {
-    bigger: false,
 }
 
 export default UserComponent;
