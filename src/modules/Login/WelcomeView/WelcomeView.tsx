24e2cafd496b68147e63b3a69cf1b26a6e51f0e0
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-19 15:05:17 +0200

    Add showing users method

--- src/modules/Login/WelcomeView/WelcomeView.tsx
+++ src/modules/Login/WelcomeView/WelcomeView.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import avatarList from '../../../common/constants/AnimalConstants';
-import { COLORS } from '../../../common/constants/ColorConstants';
 import UserComponent from '../../../common/components/UserComponent/UserComponent';
-import { getCurrentStore } from '../../../store/UsersStore';
+import { getCurrentStore } from '../../../store/UserStore';
 export interface WelcomeViewProps {
 
 }
@@ -12,16 +10,27 @@ export interface WelcomeViewState {
 }
 
 class WelcomeView extends React.Component<WelcomeViewProps, WelcomeViewState> {
-    state = { dab: 1 }
+    state = { users: null }
 
     getUsers = () => {
-        console.log(getCurrentStore())
+
+        const data = getCurrentStore()
+        const usersArray = data.users
+        const currentUsers = usersArray.map((user, index) => <UserComponent
+            avatarId={user.avatarIndex}
+            colorId={user.colorIndex}
+            username={user.username}
+            bigger={true}
+            key={index} />)
+        this.setState({ users: currentUsers })
     }
+    componentDidMount() { this.getUsers() }
     render() {
         return (
             <>
                 <p className="login__welcome-view-title">Family members</p>
                 <p className="login__welcome-view-subtitle">Tap an avatar to log in as a family member.</p>
+                <div className="login__welcome-view-container">{this.state.users}</div>
             </>
         );
     }
