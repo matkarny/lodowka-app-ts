8576bf6ebf2668b37dc7df16b7b3263daee6bbcb
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-22 09:44:12 +0200

    Add NewUserComponent

---
+++ src/common/components/NewUserComponent/NewUserComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './NewUserComponent.scss'
+export interface NewUserComponentProps {
+
+}
+
+const NewUserComponent: React.SFC<NewUserComponentProps> = props => {
+    return (  );
+}
+
+export default NewUserComponent;
