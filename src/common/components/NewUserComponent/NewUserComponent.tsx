263d870df681ff4439a2702ded2df9566dc53eab
Lukasz Brzozko <lukasz9099@gmail.com>
2019-07-22 10:03:13 +0200

    Add styles and svg

--- src/common/components/NewUserComponent/NewUserComponent.tsx
+++ src/common/components/NewUserComponent/NewUserComponent.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
-import './NewUserComponent.scss'
+import './NewUserComponent.scss';
+import { ReactComponent as AddMember } from '../../images/add-member.svg';
 export interface NewUserComponentProps {
 
 }
 
 const NewUserComponent: React.SFC<NewUserComponentProps> = props => {
-    return (  );
+    return (
+        <div className="new-user-component__items-wrapper">
+            <AddMember />
+            <p className="new-user-component__description">Add new<br />member</p>
+        </div >
+
+    );
 }
 
 export default NewUserComponent;
