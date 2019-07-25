5c87350f62643c8c7f09b242386342dcb42b375e
Mateusz Karny <mattmaister@gmail.com>
2019-07-25 13:03:19 +0200

    Author

--- src/common/components/AddNoteComponent/AddNoteComponent.tsx
+++ src/common/components/AddNoteComponent/AddNoteComponent.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { format } from 'date-fns';
 import './AddNoteComponent.scss';
+import { dropWhile } from 'lodash'
 
 import { connect } from 'react-redux';
 
@@ -8,11 +9,11 @@ import { ADD_NOTE } from '../../../store/actions/NotesActions';
 import StoreType from '../../types/StoreType';
 import { INote } from '../../interfaces/Notes';
 
-export interface IAddNoteComponentProps extends Pick<StoreType, 'notes'> {
+export interface IAddNoteComponentProps extends Pick<StoreType, 'notes' | 'users' | 'auth'> {
   addNote: (note: INote) => void;
 }
 
-const mapStateToProps = state => ({ notes: state.notes });
+const mapStateToProps = state => ({ notes: state.notes, users: state.users, auth: state.auth });
 const mapDispatchToProps = dispatch => {
   return {
     addNote: (note: INote) => dispatch({ type: ADD_NOTE, payload: note })
@@ -31,14 +32,25 @@ class AddNoteComponent extends React.Component<IAddNoteComponentProps, INote> {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+componentDidMount(){
+  this.searchForAuthor()
 
+}
   componentWillUpdate(){
     console.log(this.state)
+    
   }
 
   handleChange(event) {
     this.setState({ message: event.target.value });
     console.log(this.state)
+  }
+
+  searchForAuthor(){
+let idNumber = this.props.auth
+let number = idNumber.pop()
+let currentUser = this.props.users.find(x => x.id === number).name
+this.setState({ author: currentUser });
   }
 
   handleSubmit(event) {
