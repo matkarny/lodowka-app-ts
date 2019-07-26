b67dbbc460dc9e20ec5385ed9d21f02f6dd4503a
Mateusz Karny <mattmaister@gmail.com>
2019-07-26 12:14:48 +0200

    Final uncomplete master #SHAMEE

--- src/store/globalLocalStorage.js
+++ src/store/globalLocalStorage.js
@@ -2,7 +2,15 @@
 export const loadState = () => {
   const initialState = {
     notes: [],
-    products: [],
+    products: [{
+      name: 'Przeterminowane mleko',
+      tagPosition: { top: 160, left: 200 },
+      addedBy: 'USER X',
+      expirationDate: { year: 2019, month: 6, day: 25 },
+      id: 25160200,
+      shownPopup: false,
+      vitalityColor: ''
+    }],
     users: [],
     drawings: [],
     auth: ["-1"]
